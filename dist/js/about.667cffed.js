14ab2fee8ddd209cf0d2272121bfe6dffa7952ee
Olya Severova <34347298+olyaseverova@users.noreply.github.com>
2023-06-05 14:52:21 +0300

    'lastchangeswithdist'

---
+++ dist/js/about.667cffed.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkkamen"]=self["webpackChunkkamen"]||[]).push([[443],{381:function(n,a,e){e.r(a),e.d(a,{default:function(){return k}});var u=e(252);const t={class:"about"},r=(0,u._)("h1",null,"This is an about page",-1),s=[r];function c(n,a){return(0,u.wg)(),(0,u.iD)("div",t,s)}var i=e(744);const o={},f=(0,i.Z)(o,[["render",c]]);var k=f}}]);
+//# sourceMappingURL=about.667cffed.js.map
